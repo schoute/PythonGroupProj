1b4bcedf7171debaf9a52903bd0fccfd3d408177
schoute <^qRQ4Sa9qTx3p^>
2021-08-28 17:51:52 +0800

    test

--- coolSpider.py
+++ coolSpider.py
@@ -16,3 +16,12 @@ class coolSpider(scrapy.Spider):
             yield {
             'Image Link': x.xpath(newsel).extract_first(),
             }
+
+        # To recurse the next page
+        Page_selector = '.next a ::attr(href)'
+        next_page = response.css(Page_selector).extract_first()
+        if next_page:
+            yield scrapy.Request(
+                response.urljoin(next_page),
+                callback=self.parse
+            )
