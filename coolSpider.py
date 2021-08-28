dac263f6974544a02992f984cbd0354b77acc32f
schoute <^qRQ4Sa9qTx3p^>
2021-08-28 17:13:43 +0800

    test

--- coolSpider.py
+++ coolSpider.py
@@ -1,10 +1,18 @@
-class NewSpider(scrapy.Spider):
-    name = "new_spider"
-    start_urls = ['http://192.168.1.1/index.html']
+# import library
+import scrapy
+# Create spider class
+class coolSpider(scrapy.Spider):
+    # name of the spider
+    name = "cool_spider"
+    # website url you want to parse
+    start_urls = ['https://brickset.com/sets/year-2001']
+    # parse the website
     def parse(self, response):
+        # CSS selector for images
         css_selector = 'img'
         for x in response.css(css_selector):
             newsel = '@src'
+            # output link to image, file path, extract file
             yield {
             'Image Link': x.xpath(newsel).extract_first(),
             }
