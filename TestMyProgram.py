f1ccec61f95912b2a772f8186b3e391dbbb9ebc9
schoute <^qRQ4Sa9qTx3p^>
2021-08-29 14:40:23 +0800

    test

---
+++ TestMyProgram.py
@@ -0,0 +1,18 @@
+import unittest
+
+class TestMyProgram(unittest.TestCase):
+
+    def test_EngineType(self):
+        print("Testing")
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+#def test_upper(self):
+#self.assertEqual('foo' .upper() , 'Foo')
+#def test_isupper(self):
+#self.assertTrue('FOO' .isupper())
+#self.assertFalse('Foo'.isupper())
+
