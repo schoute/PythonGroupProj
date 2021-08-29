ac77957e59586fcfe3bcdafee5459fa1472e2868
schoute <^qRQ4Sa9qTx3p^>
2021-08-29 14:40:32 +0800

    test

--- browser.py
+++ browser.py
@@ -26,24 +26,3 @@ headers = {
 url2: str = 'http://httpbin.org/headers'
 rh = requests.get(url2, headers=headers)
 print(rh.text, )
-
-
-
-# line 29 for some reason will fetch the whole website all over again
-# need to figure out how to output the user agent part only
-# Use the Request library
-# import requests
-# # Set the target webpage
-# url = 'https://brickset.com/sets/year-2001'
-# r = requests.get(url)
-# # This will get the full page
-# print(r.text)
-# # This will modify the headers user-agent
-# headers = {
-# 'User-Agent' : 'Mobile'
-# }
-# # Test it on an external site
-# url2 = 'https://brickset.com/sets/year-2001'
-# rh = requests.get(url2, headers=headers)
-# print(rh.text)
-
