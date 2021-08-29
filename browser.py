be890cb543c8d7f9e187ba1de5bf59d9050de004
schoute <^qRQ4Sa9qTx3p^>
2021-08-29 15:35:12 +0800

    test

--- browser.py
+++ browser.py
@@ -1,28 +1,32 @@
-# # Use the Request library
+#Use the Request library
 import requests
-# # Set the target webpage
+
+# Set the target webpage
 url = 'https://brickset.com/sets/year-2001'
 r = requests.get(url)
-# # This will get the full page
+
+# This will get the full page
 print(r.text)
 
 # # This will get the status code
 print("Status code:")
 print("\t *", r.status_code)
 
-# # This will just get just the headers
+# This will just get just the headers
 h = requests.head(url)
 print("Header:")
 print("**********")
-# # This will just get just the headers
+
+# This will just get just the headers
 for x in h.headers:
     print("\t ", x, ":", h.headers[x])
 print("**********")
-# # This will modify the headers user-agent
+
+# This will modify the headers user-agent
 headers = {
     'User-Agent' : 'Mobile'
 }
-# # Test it on an external site
+# Test it on an external site
 url2: str = 'http://httpbin.org/headers'
 rh = requests.get(url2, headers=headers)
 print(rh.text, )
